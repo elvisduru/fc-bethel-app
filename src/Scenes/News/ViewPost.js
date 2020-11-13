31b72848f7d424eb2597ccd373575c335dfe0899
elvisduru <elvisduru@DESKTOP-0P7MV8G.localdomain>
2020-11-13 18:47:23 +0100

    added loader

--- src/Scenes/News/ViewPost.js
+++ src/Scenes/News/ViewPost.js
@@ -1,30 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import styles from './ViewPost.module.css'
-import { useParams } from 'react-router-dom';
-import axios from 'axios'
-import Menu from '../../containers/Menu/Menu';
-import { Footer } from '../../components/Footer/Footer';
+import React, { useState, useEffect } from "react";
+import styles from "./ViewPost.module.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Menu from "../../containers/Menu/Menu";
+import { Footer } from "../../components/Footer/Footer";
 
 export const ViewPost = (props) => {
   const [post, setPost] = useState();
 
-  const { id } = useParams()
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchPost = async () => {
-      const res = await axios.get(`/api/blog/${id}`)
-      setPost(res.data)
-    }
+      const res = await axios.get(`/api/blog/${id}`);
+      setPost(res.data);
+    };
 
-    fetchPost()
-  }, [id])
+    fetchPost();
+  }, [id]);
 
   return (
     <div className={styles.ViewPost}>
       <Menu />
       <main>
         <div className={styles.content}>
-          {post && (
+          {!post ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <div className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          ) : (
             <>
               <h3>{new Date(post.createdAt).toDateString()}</h3>
               <h1>{post.title}</h1>
@@ -32,12 +50,15 @@ export const ViewPost = (props) => {
               <div className={styles.featuredImg}>
                 <img src={post.image} alt="" />
               </div>
-              <div className={styles.body} dangerouslySetInnerHTML={{ __html: post.body }}></div>
+              <div
+                className={styles.body}
+                dangerouslySetInnerHTML={{ __html: post.body }}
+              ></div>
             </>
           )}
         </div>
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
